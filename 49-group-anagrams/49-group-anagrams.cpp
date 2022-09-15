4e463c25598f98b8be27850c6a0c4c270ba7badd
Sidharth Sundaram <sidharth.rama@gmail.com>
2022-09-15 23:29:40 +0530

    Time: 68 ms (44.55%), Space: 19.5 MB (84.04%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-            vector<vector<string>> res;
-            unordered_map<string, vector<string>>mp;
+            vector<vector<string>> res; //result vector
+            unordered_map<string, vector<string>>mp; //unordered map that will store string
             for(auto str: strs){
-                string sorted = str;
-                sort(sorted.begin(), sorted.end());
-                mp[sorted].push_back(str);
+                string sorted = str; //this will be the key to the hashmap that we are creating as we are mapping all that match with the sorted string to that key.
+                sort(sorted.begin(), sorted.end()); //sorted string as key
+                mp[sorted].push_back(str); //for every sorted key, we are pushing an iterated value in strs.
         }
         for(auto i = mp.begin(); i != mp.end(); i++){
-            res.push_back(i->second);
+            res.push_back(i->second); //we are pushing all those values that are related to the sorted index.
         }
         return res;
     }
 };
+
+//TC: O(M*NlogN);
